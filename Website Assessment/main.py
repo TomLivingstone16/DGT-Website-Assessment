240f158370ad261d30964baa0705e79c6ea70129
TomLivingstone16 <tomlivingstone@cromwell.school.nz>
2024-06-24 15:05:16 +1200

    worked more on the index page, started signup

--- Website Assessment/main.py
+++ Website Assessment/main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
 import sqlite3
-
-"""def execute_sql():
-    DATABASE = "database.db"
+def execute_sql(sql,val):
+    """This function commits sql changes to the database."""
+    DATABASE = "U:\profile\Documents\Website Assessment\Website Assessment\database.db"
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     print("beans on toast")
@@ -11,16 +11,18 @@ import sqlite3
         db.commit()
     else:
         cursor.execute(sql)
-        db.commit()"""
-
-
+        db.commit()
 app = Flask(__name__)
 
 # Home route
-@app.route('/')
+@app.route('/',methods=("GET", "POST"))
 def home():
-    return render_template('index.html')  # Return index page
+    temp = execute_sql("SELECT * FROM 'tUsers'",())
+    return render_template('index.html',temp=temp)  # Return index page
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 # Run the app
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
