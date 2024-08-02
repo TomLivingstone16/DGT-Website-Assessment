d2f716e35daa32e5e14540233695b17075b64ed8
TomLivingstone16 <tomlivingstone@cromwell.school.nz>
2024-08-02 15:03:25 +1200

    Added Image storage to database, began profile pic

--- Website Assessment/main.py
+++ Website Assessment/main.py
@@ -1,9 +1,40 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-import sqlite3
-import re
+import sqlite3, re, base64
+def get_image_from_blob():
+    blobfile = open('static/profile_blob.txt', 'r')
+    image_data = blobfile.read()
+    # Convert from a tuple to a byte
+    file = str(image_data).strip('(" ",)')
+    finalFile = file.strip("b' '")
+    print(finalFile)
+    # Decode the file
+    blob = base64.b64decode(finalFile)
+    # Create a new image file with the data from the blob
+    return blob
+def get_profile_picture(username):
+    DATABASE = "database.db"
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
 
+    # Retrieve the image
+    query = f"SELECT ProfileImage FROM tUsers WHERE [Username] = '{username}'"
+    c = cursor.execute(query)
 
+    # Fetch the data
+    image_data = c.fetchone()
+    # Convert from a tuple to a byte
+    file = str(image_data).strip('(" ",)')
+    finalFile = file.strip("b' '")
+    # Decode the file
+    blob = base64.b64decode(finalFile)
+    # Create a new image file with the data from the blob
+    text_file = open("static/"+username+".jpg", 'wb')
+    text_file.write(blob)
+    text_file.close()
 
+    print("Image retrieved and written to file.")
+
+    return "static/"+username+".jpg"
 
 def execute_sql(sql,val):
     """This function commits sql changes to the database."""
@@ -67,7 +98,7 @@ def signup():
         elif not username or not password or not email:
             msg = 'Please fill out the form!'
         else:
-            cursor.execute(f'INSERT INTO tUsers VALUES (NULL,"{username}","{password}","{email}")')
+            cursor.execute(f'INSERT INTO tUsers VALUES (NULL,"{username}","{password}","{email}",0,{get_image_from_blob()})')
             db.commit()
             msg = 'You have successfully registered !'
 
@@ -107,7 +138,9 @@ def userpage(username):
     cursor.execute(f'SELECT * FROM tUsers WHERE [Username] = "{username}"')
     user = cursor.fetchone()
     print(user[4])
-    return render_template("userpage.html",username = user[1],followerCount = user[4])
+    profile_picture = get_profile_picture(user[1])
+    print(profile_picture)
+    return render_template("userpage.html",username = user[1],followerCount = user[4],profile_picture = profile_picture)
 
 # Run the app
 if __name__ == "__main__":
